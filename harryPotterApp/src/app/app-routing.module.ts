95b6d3a03444b6ac7279852313ee5326c08f7cc0
Shawn Harris <shawn.harris3510@gmail.com>
2020-01-20 08:49:37 -0500

    routes working, correct components not rendering

--- harryPotterApp/src/app/app-routing.module.ts
+++ harryPotterApp/src/app/app-routing.module.ts
@@ -1,11 +1,66 @@
-import { NgModule } from '@angular/core';
+// import { NgModule } from '@angular/core';
+// import { Routes, RouterModule } from '@angular/router';
+
+
+// const routes: Routes = [];
+
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule]
+// })
+// export class AppRoutingModule { }
+
 import { Routes, RouterModule } from '@angular/router';
 
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { QuotesComponent } from './quotes/quotes.component';
+import { JkRowlingComponent } from './jk-rowling/jk-rowling.component';
+import { BooksComponent } from './books/books.component';
+import { MoviesComponent } from './movies/movies.component';
+import { DiningComponent } from './dining/dining.component';
+import { RidesComponent } from './rides/rides.component';
+import { ShopsComponent } from './shops/shops.component';
+import { ShowsComponent } from './shows/shows.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: '',
+        component: HomePageComponent,
+    },
+    {
+        path: 'jkRowling',
+        component: JkRowlingComponent,
+    },
+    {
+        path: 'books',
+        component: BooksComponent
+    },
+    {
+        path: 'movies',
+        component: MoviesComponent
+    },
+    {
+        path: 'dining',
+        component: DiningComponent
+    },
+    {
+        path: 'rides',
+        component: RidesComponent
+    },
+    {
+        path: 'shops',
+        component: ShopsComponent
+    },
+    {
+        path: 'shows',
+        component: ShowsComponent
+    },
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+    // otherwise redirect to home
+    { path: '**', redirectTo: '' }
+];
+
+export const AppRoutingModule = RouterModule.forRoot(routes);
