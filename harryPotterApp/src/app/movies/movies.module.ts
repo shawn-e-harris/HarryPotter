8e5e3747714fd3c9f6824871979e84e669231b1c
Shawn Harris <shawn.harris3510@gmail.com>
2020-01-20 19:53:56 -0500

    quotes component rendering, js script not working

---
+++ harryPotterApp/src/app/movies/movies.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { QuotesModule } from "../quotes/quotes.module"
+import { MoviesComponent } from "./movies.component"
+
+@NgModule({
+  declarations: [
+    MoviesComponent
+  ],
+  imports: [
+    BrowserModule,
+    QuotesModule
+  ],
+  bootstrap: [MoviesComponent]
+})
+export class AppModule { }
