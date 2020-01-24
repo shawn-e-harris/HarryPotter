3b18dcfd64afcc48717c98cc919b8032664741c1
Shawn Harris <shawn.harris3510@gmail.com>
2020-01-23 20:21:07 -0500

    angular material v8.2.3

--- harryPotterApp/src/app/app.module.ts
+++ harryPotterApp/src/app/app.module.ts
@@ -1,19 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
-import { QuotesModule } from "./quotes/quotes.module"
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppRoutingModule } from "./app-routing.module";
+import { QuotesModule } from "./quotes/quotes.module";
 
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { FooterComponent } from './footer/footer.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { JkRowlingComponent } from './jk-rowling/jk-rowling.component';
-import { BooksComponent } from './books/books.component';
-import { MoviesComponent } from './movies/movies.component';
-import { DiningComponent } from './dining/dining.component';
-import { RidesComponent } from './rides/rides.component';
-import { ShopsComponent } from './shops/shops.component';
-import { ShowsComponent } from './shows/shows.component';
+import { AppComponent } from "./app.component";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { FooterComponent } from "./footer/footer.component";
+import { HomePageComponent } from "./home-page/home-page.component";
+import { JkRowlingComponent } from "./jk-rowling/jk-rowling.component";
+import { BooksComponent } from "./books/books.component";
+import { MoviesComponent } from "./movies/movies.component";
+import { DiningComponent } from "./dining/dining.component";
+import { RidesComponent } from "./rides/rides.component";
+import { ShopsComponent } from "./shops/shops.component";
+import { ShowsComponent } from "./shows/shows.component";
+
+import { MatSliderModule } from "@angular/material/slider";
 
 @NgModule({
   declarations: [
@@ -29,12 +31,8 @@ import { ShowsComponent } from './shows/shows.component';
     ShopsComponent,
     ShowsComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    QuotesModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, QuotesModule, MatSliderModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
